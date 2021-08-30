c6396cc0aca9dd23ca82161a49993d8b0786c460
Vlad <chepurnojv@gmail.com>
2021-08-30 15:52:50 +0300

    unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className='ui container' style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
+
+//Access Key=  ZKmp6TBZohDg5wPZvxgaQso_c34xuVdgS54f8CvnnQo
+//Secret key= CSl74LvWAeZjsEqnRsT3CVDtUgN3LHeQRGYaMpA7ywk
